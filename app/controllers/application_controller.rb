9be31efc3a7b80ce24778a09559b28da80670541
EllaNancyFay <ellaschofield0@gmail.com>
2014-11-25 16:02:42 +0000

    Extracted time into variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  @time = "25-Nov-2014 15:15:00"
 end
